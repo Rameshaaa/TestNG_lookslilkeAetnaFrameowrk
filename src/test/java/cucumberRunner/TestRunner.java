f58a05e6c758eb33840ca0626302340c2b3a8a41
Rameshaaa <rammech465@gmail.com>
2020-12-26 15:27:56 +0530

    new commit 12/26

--- src/test/java/cucumberRunner/TestRunner.java
+++ src/test/java/cucumberRunner/TestRunner.java
@@ -1,11 +1,7 @@
 package cucumberRunner;
 
-import java.io.File;
 
-import org.junit.AfterClass;
 import org.junit.runner.RunWith;
-
-
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -18,10 +14,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 		features = "src/test/java/CucumberFeature",
 		glue="stepDefinitions",
 		
-		/*plugin={"pretty","html:target/cucumber", 
-				
-				}*/
-		plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+		plugin={"pretty","html:target/cucumber", 
+				})
 public class TestRunner extends AbstractTestNGCucumberTests	 {
 	
 	
